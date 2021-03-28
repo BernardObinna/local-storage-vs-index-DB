67d9c711cbf99af7329a932dbaf6902740180667
Vayne <bernardobinna2@gmail.com>
2021-03-28 15:27:56 +0100

    feature / create records - corrected date format

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,9 @@
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue';
-// import moment from 'moment'
+import moment from 'moment'
 
-// import db from '@/utils/firebase.js';
+import db from '@/utils/firebase.js';
 
 export default {
   name: 'Home',
@@ -32,22 +32,22 @@ export default {
     // });
     //
 
-    // const batch = db.batch();
+    const batch = db.batch();
 
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 1; i++) {
       const user = {
         name: this.$faker().fake('{{name.firstName}} {{name.lastName}}'),
         address: this.$faker().fake('{{address.streetAddress}}, {{address.cityName}} {{address.county}}'),
         bvn: this.$faker().datatype.number({ min: 10000000000, max: 99999999998 }),
-        dob: this.$faker().date.past(),
+        dob: moment(this.$faker().date.past()).format('Do MMMM, YYYY'),
         email: this.$faker().internet.email(),
         phone: this.$faker().fake('0810') + this.$faker().datatype.number({ min: 1000000, max: 9999998 })
       };
       this.users.push(user);
-      // const refrence = db.collection('users').doc(); // to automatically generate a unique id
-      // batch.set(refrence, user);
+      const refrence = db.collection('users').doc(); // to automatically generate a unique id
+      batch.set(refrence, user);
     }
-    //
+
     // batch.commit().then(() => {
     //   this.$toastr.s('Data uploaded');
     // });
